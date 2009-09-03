5899152382a1ce55a191664a64355e129602927a
Devyn Cairns <devyn.cairns@gmail.com>
2009-09-02 18:00:28 -0700

    Attempting more progress, this time with Runtime. Still needs the Scope class.

---
+++ etl/runtime.rb
@@ -0,0 +1,30 @@
+# English Test Language :: Runtime
+#   ETL::Runtime
+#   ETL::Scope
+#   ETL::Number
+#   ETL::Text
+
+module ETL
+  def self.number(base=0, trail=0)
+    "#{base.to_i}.#{trail.to_i}".to_f
+  end
+  def self.text(t)
+    t.to_s
+  end
+end
+
+class ETL::Runtime
+  def initialize
+    @global_scope = ETL::Scope.new
+    @global_scope.verbs.define('write', :text) do |t|
+      puts t
+    end
+  end
+  def run string
+    ETLGrammarParser.new.parse(string).eval(@global_scope)
+  end
+end
+
+class ETL::Scope
+  # unfinished
+end
