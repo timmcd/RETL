428d5c16ae941e74e246fcaf8d99901149a2514e
Devyn Cairns <devyn.cairns@gmail.com>
2009-09-12 13:43:33 -0700

    awesome skilled ness super...

--- lib/etl/pdef.rb
+++ lib/etl/pdef.rb
@@ -27,13 +27,7 @@ module ETL
 
   # rule script
   #   one or more sentences.
-  module ScriptNode 
-    def eval(scope=nil)
-      scope ||= ETL::Scope.new
-      elements.map do |s|
-        s.eval(scope)
-      end.last
-    end
+  module ScriptNode
     def compile
       IL[
           *elements.map do |e|
@@ -47,34 +41,48 @@ module ETL
   #   one or more statements,
   #   delimited by "and",
   #   ended with a period.
-  module SentenceNode 
-    def eval(scope)
-      statement.eval(scope)
-      opt.elements.map{|st|st.eval(scope)}.last
-    end
+  module SentenceNode
     def compile
-      IL[statement.compile, *opt.elements.map{|st|st.compile}]
+      IL[chunk.compile, *opt.elements.map{|st|st.chunk.compile}]
     end
   end
 
   # rule statement
   #   a verb followed by any number of arguments,
   #   delimited by a comma.
-  module StatementNode 
-    def eval(scope)
-      verb.eval(scope).call(*arguments.elements.map{|a|a.a.eval(scope)})
-    end
+  module StatementNode
     def compile
       IL[verb.compile, *arguments.elements.map{|a|a.a.compile}]
     end
   end
 
-  # rule space
-  #   any whitespace.
-  module SpaceNode 
-    def eval(scope)
-      # do nothing
+  # rule creation
+  #   create typednoun [as value].
+  module CreationNode
+    def compile
+      if as.respond_to?(:argument)
+        SP[:create => typednoun.compile, :as => as.argument.compile]
+      else
+        SP[:create => typednoun.compile]
+      end
     end
+  end
+
+  # rule typednoun
+  #   ... TODO: update docs :D
+  module TypedNounNode
+    def compile
+      if q.respond_to?(:property)
+        SP[:type => type.compile, :noun => name.text_value, :prop => q.property.compile]
+      else
+        SP[:type => type.compile, :noun => name.text_value]
+      end
+    end
+  end
+
+  # rule space
+  #   any whitespace [\s] or [ \n\t].
+  module SpaceNode
     def compile
       nil
     end
@@ -83,10 +91,7 @@ module ETL
   # rule verbcalled
   #   a statement surrounded by
   #   square brackets.
-  module VerbCalledNode 
-    def eval(scope)
-      s.eval(scope)
-    end
+  module VerbCalledNode
     def compile
       SP[:iexpr => s.compile]
     end
@@ -96,10 +101,7 @@ module ETL
   #   a word made of:
   #   - any letter [A-Za-z]
   #   - a hyphen [-]
-  module VerbNode 
-    def eval(scope)
-      scope.verbs[text_value]
-    end
+  module VerbNode
     def compile
       SP[:verb => text_value]
     end
@@ -108,53 +110,53 @@ module ETL
   # rule argument
   #   one of:
   #   - verbcalled
+  #   - assignment
   #   - noun
   #   - math
-  #   - type
   #   - number
   #   - text
   module ArgumentNode
+  end
+
+  module AssignmentNode
+    def compile
+      IL[verb.compile, SP[:from => noun.compile, :to => argument.compile]]
+    end
   end
 
   # rule noun
   #   any of [A-Za-z-] or "it",
   #   prefixed optionally by 'the',
   #   or type and optionally 'of',
-  #   followed by s or 's optionally
+  #   followed by ' or 's optionally
   #     and another noun, to designate
   #     a property.
-  module NounNode 
-    def eval(scope)
-      n = scope.nouns[name.text_value,type.eval(scope)]
-      property ? property.eval(n.scope) : n
-    end
+  module NounNode
     def compile
       if q.respond_to?(:property)
-        SP[:noun => name.text_value, :type => (r.nt.compile rescue nil), :prop => property.compile]
+        SP[:noun => name.text_value, :prop => q.property.compile]
       else
-        SP[:noun => name.text_value, :type => (r.nt.compile rescue nil)]
+        SP[:noun => name.text_value]
       end
     end
   end
 
   # rule it
   #   "it" - the last object used.
-  module ItNode 
-    def eval(scope)
-      scope.it
-    end
+  module ItNode
     def compile
-      SP[:noun => :it]
+      if q.respond_to?(:property)
+        SP[:noun => :it, :prop => q.property.compile]
+      else
+        SP[:noun => :it]
+      end
     end
   end
 
   # rule math
   #   any sequence of number & mathoperator,
   #   surrounded by parentheses.
-  module MathNode 
-    def eval(scope)
-      ETL.number(Kernel.eval("#{base.eval(scope)} #{operations.elements.map{|o|"#{o.op.eval(scope)} #{o.n.eval(scope)}"}.join(' ')}"))
-    end
+  module MathNode
     def compile
       SP[:math => "#{base.eval(scope)} #{operations.elements.map{|o|"#{o.op.eval(scope)} #{o.n.eval(scope)}"}.join(' ')}"]
     end
@@ -163,15 +165,7 @@ module ETL
   # rule mathoperator
   #   plus, minus, divide, exponent, multiply
   #      +,     -,      /,        ^,        *
-  module MathOperatorNode 
-    def eval(scope)
-      case text_value
-      when '^'
-        return "**"
-      else
-        return text_value
-      end
-    end
+  module MathOperatorNode
     def compile
       return text_value
     end
@@ -180,10 +174,7 @@ module ETL
   # rule struct
   #   any of [A-Za-z-],
   #   the name of a struct
-  module StructNode 
-    def eval(scope)
-      scope.structs[text_value]
-    end
+  module StructNode
     def compile
       SP[:struct => text_value]
     end
@@ -196,10 +187,7 @@ module ETL
   #   - text
   #   - struct
   #   - verb
-  module BuiltinTypeNode 
-    def eval(scope)
-      return text_value.to_sym
-    end
+  module BuiltinTypeNode
     def compile
       SP[:bit => text_value]
     end
@@ -211,23 +199,14 @@ module ETL
   #       create the struct game.
   #       create the game zuuup and
   #         set its description to "some random weird game".
-  module StructTypeNode 
-    def eval(scope)
-      return struct.eval(scope)
-    end
-    def compile
-      SP[:srt => struct.compile]
-    end
+  module StructTypeNode
   end
 
   # rule number
   #   any sequence of decimal digits [0-9],
   #   optionally followed by a point [.]
   #     and more decimal digits.
-  module NumberNode 
-    def eval(scope)
-      self.to_i
-    end
+  module NumberNode
     def to_i
       return ETL.number(base.text_value.to_i, point.respond_to?(:n) ? point.n.text_value.to_i : nil)
     end
@@ -242,10 +221,7 @@ module ETL
   #     including escape characters \" ["], \n [LF], \e [escape], \r [CR],
   #     or hex/octal escape characters \274, \xBC
   #     surrounded by double quotation marks ["].
-  module TextNode 
-    def eval(scope)
-      self.to_s
-    end
+  module TextNode
     def to_s
       str.text_value.
         gsub('\"', '"').
@@ -268,11 +244,8 @@ module ETL
   #   comment can come at the end of a sentence,
   #   blockcomment can come anywhere whitespace goes.
   module CommentNode
-    def eval(scope)
-      # do nothing
-    end
     def compile
-      SP[:comment => self.comment]
+      SP[:comment => self.comment.text_value]
     end
   end
 end
